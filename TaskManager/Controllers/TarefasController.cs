285d24fe1578e5611b59625a763c330003cb84fb
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2022-02-22 10:17:56 -0300

    update projeto

---
+++ TaskManager/Controllers/TarefasController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Controllers;
+
+public class TarefasController : Controller
+{
+    // GET: api/<TarefasController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<TarefasController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<TarefasController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<TarefasController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<TarefasController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
